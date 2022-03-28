d9b83bcdd1ab476930daaca045348863a362dace
jpuronah <jpuronah@student.hive.fi>
2022-03-28 17:36:44 +0300

    Mintis salee

--- board.c
+++ board.c
@@ -6,37 +6,25 @@
 /*   By: jpuronah <jpuronah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 15:29:51 by jpuronah          #+#    #+#             */
-/*   Updated: 2022/03/28 13:52:27 by jpuronah         ###   ########.fr       */
+/*   Updated: 2022/03/28 17:35:55 by jpuronah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
-
-/*
-Kaikki turha täält pois, eli ne jos h ==1 tai h == 4 lähti fittuu
-	-> on hidas mut ehk menee läpi
-
-	HUOM!
-	-	ft_set_boardissa oli hämärää paskaa, jota muutin
-
-	Yks kyssäri kans mietityttäny, et onks noi ft_strnew mallocit fine, mut kyl 
-	varmaa?
-*/
 
 static char	*ft_empty_board(char *board, size_t size)
 {
-	size_t	i;
+	size_t	index;
 
-	i = 0;
-	while (i < (size * (size + 1)))
+	index = 0;
+	while (index < (size * (size + 1)))
 	{
-		board[i] = '.';
-		if (i % (size + 1) == size)
-			board[i] = '\n';
-		i++;
+		board[index] = '.';
+		if (index % (size + 1) == size)
+			board[index] = '\n';
+		++index;
 	}
-	board[i] = '\0';
+	board[index] = '\0';
 	return (board);
 }
 
@@ -45,24 +33,21 @@ static char	*ft_set_board(char *board, char **tetri_list, size_t size)
 	char	*output;
 
 	output = NULL;
-	while (board && tetri_list && size) //onks tää parempi ehto ku while (1) ? ja onks tää normin mukaa ok?
+	while (board && tetri_list)
 	{
 		board = ft_empty_board(board, size);
 		output = ft_solve(board, tetri_list, size);
 		if (!output)
 		{
-			size++;
+			++size;
 			free(board);
-			//board = ft_strnew(size * (size + 1) + 1);			näist toine
-			board = (char *)malloc(sizeof(size * (size + 1) + 1));
+			board = ft_strnew(size * (size + 1) + 1);
 		}
 		else
-			return (output); //Täs oli ennen board
+			return (output);
 	}
 	return (NULL);
 }
-
-//Yks vaihtis ois myös initializaa boardi vast ft_set_boardis ja printtaa se sielt
 
 void	ft_board(char **tetri_list)
 {
@@ -76,9 +61,8 @@ void	ft_board(char **tetri_list)
 	while (tetri_list[n_pieces] != NULL)
 		++n_pieces;
 	while (size * size < n_pieces * 4)
-		size++;
-	//board = ft_strnew(size * (size + 1) + 1);			//näist pitäs valita
-	board = (char *)malloc(sizeof(size * (size + 1) + 1));
+		++size;
+	board = ft_strnew(size * (size + 1) + 1);
 	if (!tetri_list || !board)
 		ft_error();
 	board = ft_set_board(board, tetri_list, size);
